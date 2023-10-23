c5077fbba306a65fbba28b093c1016f49e68421f
GreynoooXD <137965161+GreynoooXD@users.noreply.github.com>
2023-10-23 23:01:37 +0800

    Add files via upload

--- BrayennnCode3.0.py
+++ BrayennnCode3.0.py
@@ -677,7 +677,7 @@ def brayen_dump():
 		for pi in koH['friends']['data']:
 			try:id.append(pi['id']+'|'+pi['name'])
 			except:continue
-		print(f' [+] Total ID Yang Terkumpul : {}'(len(id)))
+		print(f' [+] Total ID Yang Terkumpul : {}'.format(len(id)));setting()
 		setting()
 	except requests.exceptions.ConnectionError:
 		print(' [+] Internet Lu Gak Ada Anjing')
