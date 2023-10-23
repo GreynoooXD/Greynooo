e6293e1b24d2b4fd7e5d3124fa1c19ca13b6feab
GreynoooXD <137965161+GreynoooXD@users.noreply.github.com>
2023-10-23 21:26:20 +0800

    Add files via upload

--- Jhanson2.py
+++ Jhanson2.py
@@ -318,7 +318,7 @@ def nge_krek1():
 			b = ses.get("https://graph.facebook.com/{}".format(a),params = params,cookies = {'cookie': cok}).json()
 			for c in b["friends"]["data"]:
 				id.append(c["id"]+"|"+c["name"])
-			print('>> Total Idz : {h}'+str(len(id)))
+			print('>> Total Idz : {}'.format(len(id))))
 		except Exception as e:
 			print(e)
 #-------------------[ CRACK-PENGIKUT ]----------------#
